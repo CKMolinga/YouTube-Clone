9726625f20afa561732f2a2fa20c32816b67fcb5
CKMolinga <ck.molinga@gmail.com>
2022-09-23 17:50:36 +0100

    Add channel videos

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -19,8 +19,8 @@ const Feed = () => {
   },[selectedCategory])
 
   return (
-    <Stack sx={{ flexDirection: { sx: 'colum', md: "row"}}}>
-      <Box sx={{ height: { sx: 'auto', md: '92vh'}, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2} }}>
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
         <Sidebar
         selectedCategory={selectedCategory}
         setselectedCategory={setselectedCategory} />
