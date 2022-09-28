2628c2aa89982279a7d3c1bcee47ef14f30443b4
CKMolinga <ck.molinga@gmail.com>
2022-09-28 16:35:22 +0100

    Minimal change

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -19,7 +19,7 @@ const Feed = () => {
   },[selectedCategory])
 
   return (
-    <Stack sx={{ flexDirection: { sx: 'colum', md: "row"}}}>
+    <Stack sx={{ flexDirection: { sx: 'column', md: "row"}}}>
       <Box sx={{ height: { sx: 'auto', md: '92vh'}, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2} }}>
         <Sidebar
         selectedCategory={selectedCategory}
